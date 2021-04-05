e90242732538ba7ed61b20e5dd04ebba79910c6d
shssp <shishlinsv@gmail.com>
2021-04-05 21:59:44 +0400

    Hide instead of remove start page

--- StartPage.js
+++ StartPage.js
@@ -1,38 +1,24 @@
 function StartLikePresenter() {
     AddScriptElement('AdminChat.js');
-    SelfRemove();
+    SelfHide();
 }
 
 function StartLikeViewer() {
     AddScriptElement('UserChat.js');
-    document.getElementById("PresenterControl").remove();
-    SelfRemove();
+    $("#PresenterControl").remove();
+    SelfHide();
 }
 
 function AddScriptElement(src) {
-    let head = document.getElementsByTagName('head')[0];
+    let head = $('head')[0];
     let script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = src;
 
-    head.appendChild(script);
+    head.append(script);
 }
 
-function SelfRemove() {
-    document.getElementById("StartPage").remove();
-
-    let mainElement = document.getElementById('MainPage');
-    mainElement.style.display = '';
-
-    removejscssfile("StartPage.js", "js");
-}
-
-function removejscssfile(filename, filetype) {
-    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none" //determine element type to create nodelist from
-    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none" //determine corresponding attribute to test for
-    var allsuspects = document.getElementsByTagName(targetelement)
-    for (var i = allsuspects.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
-        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
-            allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-    }
+function SelfHide() {
+    $("#StartPage").hide();
+    $('#MainPage').show();
 }
